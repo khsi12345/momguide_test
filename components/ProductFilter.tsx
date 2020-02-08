b3b2be8538e7829a9720d9cb5bb1e70a6f6b32c2
khsi12345 <khsi6237@gmail.com>
2020-02-09 00:18:32 +0900

    product filter modifed

--- components/ProductFilter.tsx
+++ components/ProductFilter.tsx
@@ -6,7 +6,7 @@ const ProductFilter = () => {
     <Wrap>
       <FilterItem text="주의 사항" />
       <FilterItem text="유해 성분 부족" />
-      <FilterItem text="등급 성분 보충" />
+      <FilterItem text="등급 성분 보충" rank />
       <FilterItem text="친환경 인증 제품" />
       <FilterItem text="성분 공개 제품" />
     </Wrap>
