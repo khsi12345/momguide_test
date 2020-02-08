be6d5559b935fcf3801810897a71064ed1019d04
khsi12345 <khsi6237@gmail.com>
2020-02-08 18:52:20 +0900

    product name, count and product sorting category implemented

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,94 @@
-import React from "react";
-import Head from "next/head";
-import Nav from "../components/nav";
-
-const Home = () => (
-  <div>
-    <Head>
-      {/* <title>Home</title> */}
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
-  </div>
-);
+import React, { useState } from "react";
+import styled from "styled-components";
+const Home = () => {
+  const [sortKind, setSortKind] = useState();
+  const click = e => {
+    setSortKind(e.target.textContent);
+  };
+  return (
+    <Page>
+      <ProductWrap>
+        <PaddingWrap paddingValue={`0px 15px`}>
+          <WipDiv>
+            <PaddingWrap paddingValue={`10px`}>
+              <Wrapping>
+                <ProductKindWrap>
+                  <ProductKind>섬유유연제</ProductKind>
+                  <ProductCount>0</ProductCount>개
+                </ProductKindWrap>
+                <ProductSortWrap onClick={click}>
+                  <ProductSortBox click={sortKind === "최신순"}>
+                    <ProductSorting click={sortKind === "최신순"}>
+                      최신순
+                    </ProductSorting>
+                  </ProductSortBox>
+                  <ProductSortBox click={sortKind === "조회순"}>
+                    <ProductSorting click={sortKind === "조회순"}>
+                      조회순
+                    </ProductSorting>
+                  </ProductSortBox>
+                  <ProductSortBox click={sortKind === "별점순"}>
+                    <ProductSorting click={sortKind === "별점순"}>
+                      별점순
+                    </ProductSorting>
+                  </ProductSortBox>
+                </ProductSortWrap>
+              </Wrapping>
+            </PaddingWrap>
+          </WipDiv>
+        </PaddingWrap>
+      </ProductWrap>
+    </Page>
+  );
+};
+const Page = styled.div`
+  width: 980px;
+  margin: 70px auto;
+  overflow: hidden;
+`;
+const PaddingWrap = styled.div`
+  padding: ${props => props.paddingValue};
+`;
+const WipDiv = styled.div``;
+const ProductWrap = styled.section``;
+const Wrapping = styled.div`
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid #dadada;
+`;
+const ProductKindWrap = styled.div`
+  margin-bottom: 0.5rem;
+  font-size: 20px;
+  color: grey;
+  letter-spacing: 1px;
+  line-height: 1.2;
+  font-weight: 500;
+`;
+const ProductKind = styled.title`
+  display: inline-block;
+`;
+const ProductCount = styled.span``;
+const ProductSortWrap = styled.div``;
+const ProductSortBox = styled.div`
+  display: inline-block;
+  width: 70px;
+  height: 33px;
+  text-align: center;
+  font-size: 14px;
+  font-weight: 500;
+  letter-spacing: 1px;
+  color: #4d4d4d;
+  outline: none;
+  border-bottom: ${props => (props.click ? "1px solid #32b8a4" : "none")};
+`;
+const ProductSorting = styled.a`
+  width: 70px;
+  cursor: pointer;
+  outline: none;
+  color: ${props => (props.click ? "#4d4d4d" : "#cfcfcf")};
+  :hover {
+    color: #4d4d4d;
+  }
+`;
 
 export default Home;
