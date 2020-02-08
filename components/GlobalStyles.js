e0b5d2a8116f795f30a6d44bbf0038ea9829c03a
khsi12345 <khsi6237@gmail.com>
2020-02-08 16:18:13 +0900

    style nomalize

--- components/GlobalStyles.js
+++ components/GlobalStyles.js
@@ -2,6 +2,28 @@ import { createGlobalStyle } from "styled-components";
 import reset from "styled-reset";
 
 const GlobalStyles = createGlobalStyle`
-  ${reset}
+  ${reset};
+  * {
+    box-sizing: border-box;
+  }
+
+  a {
+    text-decoration: none;
+  }
+
+  button {
+    border: none;
+    &:focus {
+      outline: none;
+      border: none;
+    }
+  }
+
+  input {
+    border: none;
+    &:focus {
+      outline: none;
+    }
+  }
 `;
 export default GlobalStyles;
