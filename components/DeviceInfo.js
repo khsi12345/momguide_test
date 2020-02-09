4d082ae9cbf9c6d2058f0b17575620554cdce9bc
khsi12345 <khsi6237@gmail.com>
2020-02-09 14:03:22 +0900

    device size add

--- components/DeviceInfo.js
+++ components/DeviceInfo.js
@@ -1,9 +1,13 @@
 const size = {
   mobile: "320px",
-  tablet: "768px"
+  bigMobile: "414px",
+  tablet: "768px",
+  bigTablet: "1024px"
 };
 
 export const device = {
   mobile: `(min-width: ${size.mobile})`,
-  tablet: `min-width: ${size.tablet}`
+  bigMobile: `(min-width: ${size.bigMobile})`,
+  tablet: `(min-width: ${size.tablet})`,
+  bigTablet: `(min-width: ${size.bigTablet})`
 };
